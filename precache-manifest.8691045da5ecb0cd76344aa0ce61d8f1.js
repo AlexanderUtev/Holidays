527331b54f7552032a3b4c178999b90ae7680d23
Alexander Utev <sanek_utev@list.ru>
2019-10-27 23:22:02 +0500

    Updates

---
+++ precache-manifest.8691045da5ecb0cd76344aa0ce61d8f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b59ced2ddca37df625ccb6bb0f257054",
+    "url": "/holidays/index.html"
+  },
+  {
+    "revision": "5599d1a995006a96abd6",
+    "url": "/holidays/static/css/2.27cde75d.chunk.css"
+  },
+  {
+    "revision": "5599d1a995006a96abd6",
+    "url": "/holidays/static/js/2.2d855d35.chunk.js"
+  },
+  {
+    "revision": "85984a41cbc451cec687",
+    "url": "/holidays/static/js/main.7bf6f1dc.chunk.js"
+  },
+  {
+    "revision": "860101aab1a6bb0ce4c9",
+    "url": "/holidays/static/js/runtime-main.231d340b.js"
+  },
+  {
+    "revision": "39999b87febab81c8bd94ac10275db75",
+    "url": "/holidays/static/media/persik.39999b87.png"
+  }
+]);
